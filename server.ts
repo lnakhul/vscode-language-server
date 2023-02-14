334fe25b7e20834052d948f98c46dcb9e2e07997
Kyle Nakhul <lnakhul@gmail.com>
2023-02-14 10:59:22 -0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,66 +1,279 @@
-//Provide the autocompletion items from grammar keywords in proc.tmLanguage.json
-const AutoCompletionProvider = new class implements vscode.CompletionItemProvider {
-	provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): vscode.CompletionItem[] {
-		const line = document.lineAt(position.line);
-		const lineText = line.text.substring(0, position.character);
-		const match = lineText.match(/(?<=\$)\w*$/);
-		if (match) {
-			const word = match[0];
-			const completionItems = this.getCompletionItems(word);
-			return completionItems;
-		}
-		return [];
-	}
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+import {
+	createConnection,
+	TextDocuments,
+	Diagnostic,
+	DiagnosticSeverity,
+	ProposedFeatures,
+	InitializeParams,
+	DidChangeConfigurationNotification,
+	CompletionItem,
+	CompletionItemKind,
+	TextDocumentPositionParams,
+	TextDocumentSyncKind,
+	InitializeResult,
+} from 'vscode-languageserver/node';
 
-	private getCompletionItems(word: string): vscode.CompletionItem[] {
-		const completionItems: vscode.CompletionItem[] = [];
-		const keywords = this.getKeywords();
-		for (const keyword of keywords) {
-			if (keyword.startsWith(word)) {
-				const completionItem = new vscode.CompletionItem(keyword, vscode.CompletionItemKind.Keyword);
-				completionItems.push(completionItem);
+import {
+	TextDocument
+} from 'vscode-languageserver-textdocument';
+
+
+// Create a connection for the server, using Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+const connection = createConnection(ProposedFeatures.all);
+
+// Create a simple text document manager.
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+let hasDiagnosticRelatedInformationCapability = false;
+
+connection.onInitialize((params: InitializeParams) => {
+	const capabilities = params.capabilities;
+
+	// Show a message when the server is started.
+	connection.window.showInformationMessage('Server started.');
+
+	// Does the client support the `workspace/configuration` request?
+	// If not, we fall back using global settings.
+	hasConfigurationCapability = !!(
+		capabilities.workspace && !!capabilities.workspace.configuration
+	);
+	hasWorkspaceFolderCapability = !!(
+		capabilities.workspace && !!capabilities.workspace.workspaceFolders
+	);
+	hasDiagnosticRelatedInformationCapability = !!(
+		capabilities.textDocument &&
+		capabilities.textDocument.publishDiagnostics &&
+		capabilities.textDocument.publishDiagnostics.relatedInformation
+	);
+
+	const result: InitializeResult = {
+		capabilities: {
+			textDocumentSync: TextDocumentSyncKind.Incremental,
+			// Tell the client that this server supports code completion.
+			completionProvider: {
+				resolveProvider: true
 			}
 		}
-		return completionItems;
+	};
+	if (hasWorkspaceFolderCapability) {
+		result.capabilities.workspace = {
+			workspaceFolders: {
+				supported: true
+			}
+		};
+	}
+	return result;
+});
+
+// async function getSchema() {
+// 	// eslint-disable-next-line @typescript-eslint/no-var-requires
+// 	const fs = require('fs');
+// 	// eslint-disable-next-line @typescript-eslint/no-var-requires
+// 	const path = require('path');
+// 	const filePath = path.join(__dirname, '../syntaxes/bobproc.tmLanguage.json');
+// 	const schema =  fs.promises.readFile(filePath, 'utf8');
+// 	const parsedSchema = schema.then((data: string) => JSON.parse(data));
+// 	return parsedSchema;
+// }
+
+//Load schema file using Promises and then return the schema
+export function getSchema(): Promise<any> {
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	const fs = require('fs');
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	const path = require('path');
+	const filePath = path.join(__dirname, '../../syntaxes/bobproc.tmLanguage.json');
+	return new Promise((resolve, reject) => {
+		fs.readFile(filePath, 'utf8', (err: any, data: any) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(JSON.parse(data));
+			}
+		});
+	});
+}
+
+// Cache schema file
+let schema: any;
+getSchema().then((data: any) => {
+	schema = data;
+});
+
+
+connection.onInitialized(() => {
+	if (hasConfigurationCapability) {
+		// Register for all configuration changes.
+		connection.client.register(DidChangeConfigurationNotification.type, undefined);
+	}
+	if (hasWorkspaceFolderCapability) {
+		connection.workspace.onDidChangeWorkspaceFolders(_event => {
+			connection.console.log('Workspace folder change event received.');
+		});
+	}
+});
+
+
+// The example settings
+interface ExampleSettings {
+	maxNumberOfProblems: number;
+}
+
+// The global settings, used when the `workspace/configuration` request is not supported by the client.
+// Please note that this is not the case when using this server with the client provided in this example
+// but could happen with other clients.
+const defaultSettings: ExampleSettings = { maxNumberOfProblems: 1000 };
+let globalSettings: ExampleSettings = defaultSettings;
+
+// Cache the settings of all open documents
+const documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
+
+connection.onDidChangeConfiguration(change => {
+	if (hasConfigurationCapability) {
+		// Reset all cached document settings
+		documentSettings.clear();
+	} else {
+		globalSettings = <ExampleSettings>(
+			(change.settings.languageServerExample || defaultSettings)
+		);
 	}
 
-	private getKeywords(): string[] {
-		const keywords: string[] = [];
-		const grammar = require('../../proc.tmLanguage.json');
-		const repository = grammar.repository;
-		for (const key in repository) {
-			const value = repository[key];
-			if (value.patterns) {
-				for (const pattern of value.patterns) {
-					if (pattern.match) {
-						const match = pattern.match;
-						if (match.startsWith('(?i)')) {
-							const keyword = match.substring(4);
-							keywords.push(keyword);
-						}
-					}
+	// Revalidate all open text documents
+	documents.all().forEach(validateTextDocument);
+});
+
+function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
+	if (!hasConfigurationCapability) {
+		return Promise.resolve(globalSettings);
+	}
+	let result = documentSettings.get(resource);
+	if (!result) {
+		result = connection.workspace.getConfiguration({
+			scopeUri: resource,
+			section: 'languageServerExample'
+		});
+		documentSettings.set(resource, result);
+	}
+	return result;
+}
+
+// Only keep settings for open documents
+documents.onDidClose(e => {
+	documentSettings.delete(e.document.uri);
+});
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent(change => {
+	validateTextDocument(change.document);
+});
+
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+	// In this simple example we get the settings for every validate run.
+	const settings = await getDocumentSettings(textDocument.uri);
+
+	// The validator creates diagnostics for all keywords from the schema file
+	const text = textDocument.getText();
+	const pattern = /\b([a-zA-Z0-9_]+)\b/g;
+	let m: RegExpExecArray | null;
+
+	let problems = 0;
+	const diagnostics: Diagnostic[] = [];
+	while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
+		problems++;
+		const keyword = m[0];
+		const keywordIndex = m.index;
+		const keywordLength = keyword.length;
+		const keywordRange = {
+			start: textDocument.positionAt(keywordIndex),
+			end: textDocument.positionAt(keywordIndex + keywordLength)
+		};
+		const diagnostic: Diagnostic = {
+			severity: DiagnosticSeverity.Warning,
+			range: keywordRange,
+			message: `${keyword} is not a valid keyword`,
+			source: 'ex'
+		};
+		if (hasDiagnosticRelatedInformationCapability) {
+			diagnostic.relatedInformation = [
+				{
+					location: {
+						uri: textDocument.uri,
+						range: Object.assign({}, keywordRange)
+					},
+					message: 'Spelling matters'
+				},
+				{
+					location: {
+						uri: textDocument.uri,
+						range: Object.assign({}, keywordRange)
+					},
+					message: 'Particularly for names'
+				}
+			];
+		}
+		diagnostics.push(diagnostic);
+	}
+}
+
+
+
+
+// This handler provides the list of the completion items return from the getSchema function and caches the schema file
+connection.onCompletion(
+	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+		const items: CompletionItem[] = [];
+		if (schema) {
+			for (const key in schema) {
+				// eslint-disable-next-line no-prototype-builtins
+				if (schema.hasOwnProperty(key)) {
+					const element = schema[key];
+					const item: CompletionItem = {
+						label: key,
+						kind: CompletionItemKind.Text,
+						data: 1
+					};
+					items.push(item);
 				}
 			}
 		}
-		return keywords;
-	}
-};
-
-// This handler provides the initial list of the completion items from AutoCompletionProvider
-connection.onCompletion(
-	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-		const document = documents.get(_textDocumentPosition.textDocument.uri);
-		if (document) {
-			const position = _textDocumentPosition.position;
-			const line = document.lineAt(position.line);
-			const lineText = line.text.substring(0, position.character);
-			const match = lineText.match(/(?<=\$)\w*$/);
-			if (match) {
-				const word = match[0];
-				const completionItems = AutoCompletionProvider.getCompletionItems(word);
-				return completionItems;
-			}
-		}
-		return [];
+		return items;
 	}
 );
+
+
+// This handler resolves additional information for the item selected in the completion list (hover)
+connection.onCompletionResolve(
+	(item: CompletionItem): CompletionItem => {
+		if (schema && item.data === 1) {
+			item.detail = schema[item.label].description;
+			item.documentation = schema[item.label].description;
+		}
+		return item;
+	}
+);
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
+
+
+
+							
+					
+	
+
+
+
+

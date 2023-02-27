e17a785927f35032e909f750a0ff2c14dcd7da9e
Kyle Nakhul <lnakhul@gmail.com>
2023-02-27 18:01:59 -0500

    Update server.test.ts

--- server.test.ts
+++ server.test.ts
@@ -1,85 +1,88 @@
-import { createConnection } from 'vscode-languageserver/node';
-import { TextDocuments } from 'vscode-languageserver-textdocument';
-import { CompletionProvider } from './completion';
 import {
-  initializeServer,
-  onDidChangeConfiguration,
-  getDocumentSettings,
-  validateTextDocument,
-} from './server';
+  createConnection,
+  TextDocuments,
+  Diagnostic,
+  DiagnosticSeverity,
+  ProposedFeatures,
+  InitializeParams,
+  DidChangeConfigurationNotification,
+  CompletionItem,
+  TextDocumentPositionParams,
+  TextDocumentSyncKind,
+  InitializeResult,
+  IPCMessageReader,
+  IPCMessageWriter,
+} from 'vscode-languageserver/node';
 
-jest.mock('vscode-languageserver/node', () => ({
-  createConnection: jest.fn(),
-}));
+import {
+  TextDocument
+} from 'vscode-languageserver-textdocument';
+import { CompletionProvider } from './completion';
 
-jest.mock('vscode-languageserver-textdocument', () => ({
-  TextDocuments: jest.fn(),
-}));
+import { validateTextDocument } from './server';
 
-jest.mock('./completion', () => ({
-  CompletionProvider: jest.fn(),
-}));
+jest.mock('vscode-languageserver/node');
+jest.mock('vscode-languageserver-textdocument');
+jest.mock('./completion');
 
-describe('initializeServer', () => {
+describe('server', () => {
   const connection = createConnection();
-  const documents = new TextDocuments();
+  const documents = new TextDocuments(TextDocument);
   const completionProvider = new CompletionProvider();
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  it('registers onInitialize callback', () => {
-    initializeServer(connection, documents, completionProvider);
-    expect(connection.onInitialize).toHaveBeenCalledTimes(1);
+  describe('validateTextDocument', () => {
+    it('should validate a text document', async () => {
+      const settings = { maxNumberOfProblems: 1000 };
+      const uri = 'file:///example.txt';
+      const text = 'some text with a keyword';
+      const document = new TextDocument(uri, 'plaintext', 1, text);
+      const getDocumentSettings = jest.fn(() => Promise.resolve(settings));
+      const expectedResult = [{
+        severity: DiagnosticSeverity.Warning,
+        range: {
+          start: { line: 0, character: 17 },
+          end: { line: 0, character: 24 }
+        },
+        message: 'keyword is not a valid keyword',
+        source: 'ex'
+      }];
+      const spy = jest.spyOn(documents, 'all').mockReturnValue([document]);
+      const result = await validateTextDocument(document);
+      expect(getDocumentSettings).toHaveBeenCalledWith(uri);
+      expect(result).toBeUndefined();
+      expect(connection.sendDiagnostics).toHaveBeenCalledWith({
+        uri,
+        diagnostics: expectedResult
+      });
+    });
+
+    it('should return if there are no problems', async () => {
+      const settings = { maxNumberOfProblems: 1000 };
+      const uri = 'file:///example.txt';
+      const text = 'some text without any keywords';
+      const document = new TextDocument(uri, 'plaintext', 1, text);
+      const getDocumentSettings = jest.fn(() => Promise.resolve(settings));
+      const spy = jest.spyOn(documents, 'all').mockReturnValue([document]);
+      const result = await validateTextDocument(document);
+      expect(getDocumentSettings).toHaveBeenCalledWith(uri);
+      expect(result).toBeUndefined();
+      expect(connection.sendDiagnostics).not.toHaveBeenCalled();
+    });
   });
 
-  it('registers onDidChangeConfiguration callback', () => {
-    initializeServer(connection, documents, completionProvider);
-    expect(connection.onDidChangeConfiguration).toHaveBeenCalledTimes(1);
-  });
-
-  it('registers onDidChangeWorkspaceFolders callback', () => {
-    initializeServer(connection, documents, completionProvider);
-    expect(connection.workspace.onDidChangeWorkspaceFolders).toHaveBeenCalledTimes(1);
-  });
-});
-
-describe('onDidChangeConfiguration', () => {
-  const connection = createConnection();
-  const documents = new TextDocuments();
-  const completionProvider = new CompletionProvider();
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it('calls getDocumentSettings for every open document', () => {
-    const document1 = { uri: 'document1' };
-    const document2 = { uri: 'document2' };
-    documents.set(document1.uri, document1 as any);
-    documents.set(document2.uri, document2 as any);
-
-    onDidChangeConfiguration(connection, documents, completionProvider);
-
-    expect(getDocumentSettings).toHaveBeenCalledWith(document1.uri);
-    expect(getDocumentSettings).toHaveBeenCalledWith(document2.uri);
-  });
-});
-
-describe('validateTextDocument', () => {
-  const document = {
-    uri: 'document',
-    getText: jest.fn().mockReturnValue('some text'),
-  } as any;
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it('calls getDocumentSettings', async () => {
-    await validateTextDocument(document, {} as any);
-
-    expect(getDocumentSettings).toHaveBeenCalledWith(document.uri);
+  describe('onDidChangeConfiguration', () => {
+    it('should revalidate all open text documents', () => {
+      const change = { settings: { languageServerExample: {} } };
+      const validateTextDocument = jest.fn();
+      const spy = jest.spyOn(documents, 'all').mockReturnValue([
+        { uri: 'file:///example.txt' }
+      ]);
+      const result = connection.onDidChangeConfiguration(change);
+      expect(validateTextDocument).toHaveBeenCalledWith({ uri: 'file:///example.txt' });
+    });
   });
 });

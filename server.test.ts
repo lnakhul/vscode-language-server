4daef43c873f07546b0a7125ca7387fb9bcf2abb
Kyle Nakhul <lnakhul@gmail.com>
2023-02-27 23:45:38 -0500

    Update server.test.ts

--- server.test.ts
+++ server.test.ts
@@ -1,20 +1,48 @@
-import { TextDocument } from 'vscode-languageserver-textdocument';
+import { createConnection } from 'vscode-languageserver/node';
+import { CompletionProvider } from './completion';
+import { TextDocuments } from 'vscode-languageserver';
 import { validateTextDocument } from './server';
-import { createMockConnection } from 'vscode-languageserver/lib/test/mockConnection';
 
-test('validateTextDocument', async () => {
-  // Define a mock text document
-  const textDocument = TextDocument.create('file:///test.txt', 'plaintext', 1, 'hello world');
+jest.mock('vscode-languageserver/node', () => ({
+  createConnection: jest.fn(),
+}));
 
-  // Define a mock connection
-  const connection = createMockConnection();
+jest.mock('./completion', () => ({
+  CompletionProvider: jest.fn().mockImplementation(() => ({
+    provideCompletionItems: jest.fn(),
+  })),
+}));
 
-  // Mock the getConfiguration function
-  connection.workspace.getConfiguration = jest.fn(() => Promise.resolve({ maxNumberOfProblems: 2 }));
+jest.mock('vscode-languageserver', () => ({
+  TextDocuments: jest.fn().mockImplementation(() => ({
+    onDidChangeContent: jest.fn(),
+    onDidClose: jest.fn(),
+    all: jest.fn().mockReturnValue([]),
+  })),
+}));
 
-  // Call the validateTextDocument function
-  await validateTextDocument(textDocument, connection);
+describe('server', () => {
+  let completionProvider: CompletionProvider;
+  let documents: TextDocuments;
+  let connection: any;
 
-  // Expect the diagnostics to have been published
-  expect(connection.sendDiagnostics).toHaveBeenCalledTimes(1);
+  beforeEach(() => {
+    connection = createConnection();
+    completionProvider = new CompletionProvider();
+    documents = new TextDocuments();
+  });
+
+  it('should validate the text document', async () => {
+    const textDocument = {
+      getText: () => 'some text with a keyword',
+      uri: 'some-uri',
+      positionAt: jest.fn().mockReturnValue({ line: 0, character: 0 }),
+    };
+
+    const validateSpy = jest.spyOn(server, 'validateTextDocument');
+
+    await validateTextDocument(textDocument as any);
+
+    expect(validateSpy).toHaveBeenCalledWith(textDocument as any);
+  });
 });

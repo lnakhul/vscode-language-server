5b3ed61fde698b0f08ce64d0b06fcd8e101f2c98
Kyle Nakhul <lnakhul@gmail.com>
2023-02-27 23:38:23 -0500

    Update server.test.ts

--- server.test.ts
+++ server.test.ts
@@ -1,90 +1,20 @@
-import { createConnection, ProposedFeatures } from 'vscode-languageserver/node';
 import { TextDocument } from 'vscode-languageserver-textdocument';
-import { CompletionProvider } from './completion';
-import { initializeServer } from './server';
+import { validateTextDocument } from './server';
+import { createMockConnection } from 'vscode-languageserver/lib/test/mockConnection';
 
-jest.mock('vscode-languageserver/node', () => {
-  const createConnection = jest.fn();
-  const ProposedFeatures = jest.fn();
-  return { createConnection, ProposedFeatures };
-});
+test('validateTextDocument', async () => {
+  // Define a mock text document
+  const textDocument = TextDocument.create('file:///test.txt', 'plaintext', 1, 'hello world');
 
-jest.mock('vscode-languageserver-textdocument', () => {
-  const TextDocument = jest.fn();
-  return { TextDocument };
-});
+  // Define a mock connection
+  const connection = createMockConnection();
 
-jest.mock('./completion', () => {
-  const CompletionProvider = jest.fn();
-  return { CompletionProvider };
-});
+  // Mock the getConfiguration function
+  connection.workspace.getConfiguration = jest.fn(() => Promise.resolve({ maxNumberOfProblems: 2 }));
 
-describe('initializeServer', () => {
-  let connection;
-  let documents;
-  let completionProvider;
-  let hasConfigurationCapability;
-  let hasWorkspaceFolderCapability;
-  let hasDiagnosticRelatedInformationCapability;
+  // Call the validateTextDocument function
+  await validateTextDocument(textDocument, connection);
 
-  beforeEach(() => {
-    connection = {
-      onInitialize: jest.fn(),
-      onInitialized: jest.fn(),
-      onDidChangeConfiguration: jest.fn(),
-      client: {
-        register: jest.fn(),
-      },
-      workspace: {
-        onDidChangeWorkspaceFolders: jest.fn(),
-        getConfiguration: jest.fn(),
-      },
-      console: {
-        log: jest.fn(),
-      },
-    };
-    createConnection.mockReturnValue(connection);
-    documents = {
-      onDidClose: jest.fn(),
-      onDidChangeContent: jest.fn(),
-      all: jest.fn(),
-    };
-    TextDocument.mockReturnValue(documents);
-    completionProvider = {
-      onCompletion: jest.fn(),
-      onCompletionResolve: jest.fn(),
-    };
-    CompletionProvider.mockReturnValue(completionProvider);
-    hasConfigurationCapability = false;
-    hasWorkspaceFolderCapability = false;
-    hasDiagnosticRelatedInformationCapability = false;
-  });
-
-  afterEach(() => {
-    jest.resetAllMocks();
-  });
-
-  it('should initialize the server', () => {
-    const params = {
-      capabilities: {
-        workspace: {},
-        textDocument: {
-          publishDiagnostics: {},
-        },
-      },
-    };
-    initializeServer();
-    expect(createConnection).toHaveBeenCalledWith(
-      expect.anything(),
-      expect.anything(),
-      ProposedFeatures,
-    );
-    expect(connection.onInitialize).toHaveBeenCalled();
-    expect(connection.onInitialized).toHaveBeenCalled();
-    expect(connection.onDidChangeConfiguration).toHaveBeenCalled();
-    expect(connection.client.register).toHaveBeenCalled();
-    expect(connection.workspace.onDidChangeWorkspaceFolders).toHaveBeenCalled();
-    expect(TextDocument).toHaveBeenCalledWith(TextDocument);
-    expect(CompletionProvider).toHaveBeenCalledWith();
-  });
+  // Expect the diagnostics to have been published
+  expect(connection.sendDiagnostics).toHaveBeenCalledTimes(1);
 });

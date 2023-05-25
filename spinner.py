4b0bdda130e160dbc91d52bef49426fa229a0d2a
Kyle Nakhul <lnakhul@gmail.com>
2023-05-25 15:47:57 -0400

    Update spinner.py

--- spinner.py
+++ spinner.py
@@ -9,43 +9,31 @@ class Spinner:
         self._loading = False
         self._spin_symbols = ['|', '/', '-', '\\']
         self._thread = None
-        self._lock = threading.Lock()
 
     def _spin(self):
         write, flush = sys.stdout.write, sys.stdout.flush
         for char in itertools.cycle(self._spin_symbols):
-            with self._lock:
-                if not self._loading:
-                    break
             status = char + ' ' + self._message
             write(status)
             flush()
             write('\x08' * len(status))
             time.sleep(.1)
-        write(' ' * len(status) + '\x08' * len(status))
+            if not self._loading:
+                break
+
+        self.reset()
 
     def start(self):
-        with self._lock:
-            if self._thread is not None:
-                return
-            self._loading = True
-            self._thread = threading.Thread(target=self._spin)
-            self._thread.start()
+        self._loading = True
+        self._thread = threading.Thread(target=self._spin)
+        self._thread.start()
 
     def stop(self):
-        with self._lock:
-            self._loading = False
-            if self._thread is not None:
-                self._thread.join()
-                self._thread = None
+        self._loading = False
 
-if __name__ == '__main__':
-    spinner = Spinner('Loading data...')
-    try:
-        spinner.start()
-        # Simulating long running task
-        time.sleep(5)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        spinner.stop()
+    def reset(self):
+        write, flush = sys.stdout.write, sys.stdout.flush
+        status = ' ' * len(self._message)
+        write(' ' * len(status) + '\x08' * len(status))
+        flush()
+

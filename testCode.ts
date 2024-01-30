b2190a5eb670157e4d193130103b6a6e730ecfe2
Kyle Nakhul <lnakhul@gmail.com>
2024-01-30 15:45:31 -0500

    Update testCode.ts

--- testCode.ts
+++ testCode.ts
@@ -11,3 +11,28 @@ const groupedResults = testResults.reduce((acc, result) => {
         acc[result.testModule][result.className][result.label].push(result);
         return acc;
       }, {});
+
+ function TestResultsComponent({ data }) {
+    if (!data) {
+      return null;
+    }
+  
+    if (data instanceof TestEventResult) {
+      return (
+        <div>
+          {data.methodName}: {data.success ? "Passed" : "Failed"}
+          {/* Render other details from TestEventResult as needed */}
+        </div>
+      );
+    }
+  
+    return (
+      <div>
+        {Object.keys(data).map(key => (
+          <div key={key}>
+            <strong>{key}</strong>
+            <TestResultsComponent data={data[key]} />
+          </div>
+        ))}
+      </div>
+    )

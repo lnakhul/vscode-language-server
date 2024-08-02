c30256c486d6e72e7f545a3de16e0e73bddbe474
Kyle Nakhul <lnakhul@gmail.com>
2024-08-02 14:38:24 -0400

    Update glimpseSlickGrid.tsx

--- glimpseSlickGrid.tsx
+++ glimpseSlickGrid.tsx
@@ -1,100 +1,36 @@
-import React, { useContext, useState } from 'react';
-import { SlickgridReact, Column, GridOption } from 'slickgrid-react';
-import { v4 as uuidv4 } from 'uuid';
-import { StyleSheet, css } from 'aphrodite';
-import { VSCodePanels, VSCodePanelTab, VSCodePanelView } from '@vscode/webview-ui-toolkit/react';
-import VsCodeExtensionContext, { renderIntoWebview } from '../components/VsCodeExtensionContext';
-import FormLabel from '../components/FormLabel';
-import { ReviewHistory, ReactViewProps, ReviewSummaryProps } from '../interfaces/interfaces';
+const pathGridColumns: Column<FileRowData>[] = [
+        { id: 'path', name: 'Path', field: 'Path', sortable: true, formatter: clickablePathFormatter },
+        { id: 'rev', name: 'Rev', field: 'Rev', sortable: true },
+        { id: 'diffType', name: 'Diff Type', field: 'DiffType', sortable: true },
+        { id: 'diffAgainst', name: 'Diff Against', field: 'DiffRev', sortable: true },
+        { id: 'iml', name: 'IML', field: 'iml', sortable: true, formatter: imlFormatter }
+    ];
 
-const styles = StyleSheet.create({
-  descriptionHeader: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    marginTop: '5px',
-    marginBottom: '5px',
-  },
-  title: {
-    margin: '5px 10px',
-  },
-});
+    const pathGridDataset = convertPathInfoToFileRowData(pathInfos);
 
-interface ReviewHistoryWithId extends ReviewHistory {
-  id?: string;
-}
+    const pathGridOptions: GridOption = {
+        enableCellNavigation: true,
+        enableColumnReorder: false,
+        enableSorting: true,
+        enableHeaderMenu: false,
+        autoHeight: true,
+        autoResize: {
+            containerId: 'grid-container',
+            rightPadding: 15
+        }
+    };
 
-type ReviewHistoryRowProp = {
-  history: ReviewHistory[];
-};
-
-const ReviewHistoryRow: React.FC<ReviewHistoryRowProp> = ({ history }) => {
-  const columns: Column[] = [
-    { id: 'status', name: 'Status', field: 'status' },
-    { id: 'reviewId', name: 'Id', field: 'reviewId', formatter: (_, __, value) => `<a href="#">${value}</a>` },
-    { id: 'description', name: 'Description', field: 'description' },
-    { id: 'requestedDate', name: 'Date', field: 'requestedDate' },
-    { id: 'requestedBy', name: 'Author', field: 'requestedBy' },
-    { id: 'files', name: 'Files', field: 'files', formatter: (_, __, value) => value.map((file: any) => `${file.path} ${file.revision}`).join('\n') },
-  ];
-
-  const dataset: ReviewHistoryWithId[] = history.map(h => ({
-    ...h,
-    reviewId: h.iscurrent ? 'CURRENT' : h.reviewId,
-    id: h.id ? h.id : uuidv4(),
-  }));
-
-  const gridOptions: GridOption = {
-    enableCellNavigation: true,
-    enableColumnReorder: false,
-    enableSorting: true,
-  };
-
-  return (
-    <SlickgridReact
-      gridId="reviewHistoryGrid"
-      columnDefinitions={columns}
-      dataset={dataset}
-      gridOptions={gridOptions}
-    />
-  );
-};
-
-type ReviewHistoryProp = {
-  history: ReviewHistory[];
-};
-
-const ReviewHistoryTable: React.FC<ReviewHistoryProp> = ({ history }) => {
-  return <ReviewHistoryRow history={history} />;
-};
-
-export function VsCodeReviewSummary({ initialData }: ReactViewProps<ReviewSummaryProps>): React.ReactElement {
-  const vscodeApi = useContext(VsCodeExtensionContext);
-  const { reviewId, history = [] } = initialData;
-  const [currentTab, setCurrentTab] = useState<string>('tab_comments');
-  const onClickTab = (ev: any) => {
-    const element = ev.target as any;
-    if (element) {
-      setCurrentTab(element.id);
+    function clickablePathFormatter(row: number, cell: number, value: string, columnDef: Column<FileRowData>, dataContext: FileRowData): string {
+        return `<a href="#" data-id="${dataContext.Path}" class="clickable-path">${value}</a>`;
     }
-  };
 
-  return (
-    <div>
-      <div className={css(styles.descriptionHeader)}>
-        <FormLabel style={styles.title}>Review Summary</FormLabel>
-      </div>
-      <VSCodePanels activeId={currentTab}>
-        <VSCodePanelTab id="tab_comments" onClick={onClickTab}>Comments</VSCodePanelTab>
-        <VSCodePanelTab id="tab_history" onClick={onClickTab}>Review History</VSCodePanelTab>
-        <VSCodePanelView id="tab_comments">
-          {/* Add your comments tab content here */}
-        </VSCodePanelView>
-        <VSCodePanelView id="tab_history">
-          <ReviewHistoryTable history={history} />
-        </VSCodePanelView>
-      </VSCodePanels>
-    </div>
-  );
-}
+    function imlFormatter(row: number, cell: number, value: boolean): string {
+        return `<input type="checkbox" disabled ${value ? 'checked' : ''} />`;
+    }
 
-renderIntoWebview(<VsCodeReviewSummary initialData={window.initialData} key={window.id}></VsCodeReviewSummary>);
+    const onCellClicked = useCallback((e: Event, args: any) => {
+        const item = args?.item;
+        if (item) {
+            onPathClick(item);
+        }
+    }, []);

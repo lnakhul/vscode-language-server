aaf0ab67d59bf386abeaf756ae3d56204be9e5ba
Kyle Nakhul <lnakhul@gmail.com>
2025-01-24 14:13:13 -0500

    Update testCode.ts

--- testCode.ts
+++ testCode.ts
@@ -1,15 +1,28 @@
-public async generateSphinxDocsChoice(): Promise<void> {
-        const selection = await vscode.window.showQuickPick(
-            [
-                { label: 'Generate in WebView', value: 'webview' },
-                { label: 'Generate in Browser', value: 'browser' }
-            ],
-            {
-                placeHolder: 'How would you like to generate the Sphinx doc?'
+useEffect(() => {
+        // Load grid state from the central Map
+        const savedState = loadGridStateFromMap(griId);
+        if (gridRef.current && savedState) {
+            gridRef.current.columnDefinitions = savedState.columns;
+            gridRef.current.gridOptions = { ...gridRef.current.gridOptions, ...savedState.options };
+        }
+
+        const grid = gridRef.current?.slickGridInstance;
+
+        // Subscribe to grid state changes
+        const handleGridStateChanged = (e: CustomEvent<{ gridState: GridState; change: { type: string; newValues: any } }>) => {
+            const { gridState, change } = e.detail;
+            if (change.type === "columns") {
+                const state = {
+                    columns: gridState.columns,
+                    options: gridRef.current?.gridOptions,
+                };
+                saveGridStateToMap(griId, state);
             }
-        );
-        if (!selection) return;
-        await vscode.commands.executeCommand(
-            selection.value === 'webview' ? 'quartz.generateSphinxDocs' : 'quartz.generateSphinxDocsInBrowser'
-        );
-    }
+        };
+
+        grid?.onColumnsResized.subscribe(handleGridStateChanged);
+
+        return () => {
+            grid?.onColumnsResized.unsubscribe(handleGridStateChanged);
+        };
+    }, [rows]);
